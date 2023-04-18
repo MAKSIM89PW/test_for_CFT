21e99568ae01a46436ad3718b3ef974d7653cb45
123 <123>
2023-04-18 17:33:43 +0300

    ToDoList

---
+++ Core Data/TableViewCell.swift
@@ -0,0 +1,29 @@
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UITextView!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var editButton: UIButton!
+    @IBOutlet weak var deleteButton: UIButton!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    func setNote(note: Note) {
+        titleLabel.text = note.title
+        descriptionLabel.text = note.details
+        
+        let date = DateFormatter()
+        date.dateFormat = "MM/dd/yy/ h:mm a"
+        let dateSaved = date.string(from: note.date as! Date)
+        dateLabel.text = dateSaved
+    }
+    
+
+
+}
